524368b7d3660a978bcf4c8091c7a54010ca4c82
pArtour <edvardi72@gmail.com>
2020-04-06 20:41:02 +0300

    header

---
+++ src/blocks/modules/main/main.js
@@ -0,0 +1,33 @@
+// const mySwiper = new Swiper ('.swiper-container', {
+//     // Optional parameters
+//     direction: window.innerWidth < 440 ? 'horizontal' : 'vertical',
+//     loop: true,
+
+//     // If we need pagination
+//     pagination: {
+//       el: '.swiper-pagination',
+//       type: window.innerWidth < 440 ? 'custom' : 'bullets' ,
+//       clickable: true,
+//       renderBullet: function (index, className) {
+//         return '<span class="' + className + '">' + '0' + (index + 1) + '</span>';
+//       },
+//       renderCustom: function (swiper, current) {
+//         return '0' + current ;
+//       },
+//       hideOnClick: false
+//     },
+
+//     // Navigation arrows
+//     navigation: {
+//       nextEl: '.swiper-button-next',
+//       prevEl: '.swiper-button-prev',
+//     },
+
+//     // And if we need scrollbar
+//     scrollbar: {
+//       el: '.swiper-scrollbar',
+//       hide: true
+//     },
+//   })
+
+// console.log('h1');
